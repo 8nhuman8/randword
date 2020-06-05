d2108c0267de47a5a95744a31ab8dd85bb2f6dc9
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-06-05 16:19:39 +0300

    Update setup.py and fix package structure

--- rand-word/setup.py
+++ rand-word/setup.py
@@ -5,19 +5,22 @@ setup(
     name='rand-word',
     description='A Python package for generation random English words',
     keywords=['generation', 'word', 'random', 'english', 'python', 'package'],
-    version='0.1',
+    version='0.1.1',
 
     author='Artyom Bezmenov (8nhuman)',
     author_email='artem.bezmenov02@gmail.com',
+    license='MIT',
 
     url='https://github.com/8nhuman8/rand-word',
-    license='MIT',
+    download_url='https://github.com/8nhuman8/rand-word/archive/0.1.1.tar.gz',
 
     package_data={'': ['*.txt']},
 
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Development Status :: 1 - Planning'
+        'Development Status :: 1 - Planning',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers'
     ]
 )
