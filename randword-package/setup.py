ecb0197b9884a01db16e770007054d1c4d537034
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-06-06 13:10:50 +0300

    Add project links
    Update '.gitignore'.

--- randword-package/setup.py
+++ randword-package/setup.py
@@ -5,18 +5,24 @@ setup(
     name='randword',
     description='A Python package for generation random English words',
     keywords=['generation', 'word', 'random', 'english'],
-    version='2.0.1',
+    version='2.0.2',
 
     author='Artyom Bezmenov (8nhuman)',
     author_email='artem.bezmenov02@gmail.com',
     license='MIT',
 
-    url='https://github.com/8nhuman8/randword',
-    download_url='https://github.com/8nhuman8/randword/archive/2.0.1.tar.gz',
-
     packages=['randword'],
-
     include_package_data=True,
+
+    project_urls= {
+        'Homepage': 'https://github.com/8nhuman8/randword',
+        'Download': 'https://github.com/8nhuman8/randword/archive/2.0.2.tar.gz',
+        'Source Code': 'https://github.com/8nhuman8/randword',
+        'Bug Tracker': 'https://github.com/8nhuman8/randword/issues'
+    },
+
+    #url='https://github.com/8nhuman8/randword',
+    #download_url='https://github.com/8nhuman8/randword/archive/2.0.1.tar.gz',
 
     classifiers=[
         'Programming Language :: Python :: 3',
