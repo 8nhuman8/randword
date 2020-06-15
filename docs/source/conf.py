4d92170edd4c6ba6037890b08bfcb3edf7d7d33c
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-06-15 16:50:50 +0300

    Update .readthedocs.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
     'sphinx.ext.coverage',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
