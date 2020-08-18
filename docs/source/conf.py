e9df939f158716cac47253ddd16844960b12a45c
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-08-18 09:59:38 +0300

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ project = 'randword'
 copyright = '2020, Artyom Bezmenov'
 author = 'Artyom Bezmenov'
 
-release = '2.9.3'
+release = '2.9.4'
 
 
 # -- General configuration ---------------------------------------------------
