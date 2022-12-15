9c5f9a092513e08335ecf6429cf251d87fd8f3f0
8nhuman <artem.bezmenov02@gmail.com>
2022-12-15 23:07:39 +0300

    Test update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ project = 'randword'
 copyright = '2022, Artyom Bezmenov'
 author = 'Artyom Bezmenov'
 
-release = '2.10'
+release = '2.10.1'
 
 
 # -- General configuration ---------------------------------------------------
