2266ccd779a1450a611313da17584817a74b7b22
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2025-08-12 12:10:37 +0300

    Migrate packaging from setup.py to pyproject.toml, remove pkg_resources usage

--- randword/utilities.py
+++ randword/utilities.py
@@ -1,15 +1,15 @@
-from pkg_resources import resource_filename
 from typing import List
-from pathlib import Path
+import importlib.resources
 
 
 def get_data(folder: str, filename: str) -> List[str]:
     words = []
-    data_path = Path(resource_filename('randword', 'data'))
-    filepath = data_path / folder / f'{filename}.txt'
 
-    with open(filepath, 'r') as pos_file:
-        pos_words = pos_file.readlines()
-        words.extend(pos_words)
+    with importlib.resources.path('randword', 'data') as data_path:
+        filepath = data_path / folder / f'{filename}.txt'
+
+        with open(filepath, 'r', encoding='utf-8') as pos_file:
+            pos_words = pos_file.readlines()
+            words.extend(pos_words)
 
     return [word.rstrip() for word in words]
